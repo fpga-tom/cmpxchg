da3fe3b5bada9a1a1dc70d22c94d90558db09be2
Krajco Tomas <Tomas.Krajco@andritz.com>
2019-02-04 17:23:11 +0100

    udp

---
+++ UDPSink.cpp
@@ -0,0 +1,5 @@
+//
+// Created by tomas on 2/4/19.
+//
+
+#include "UDPSink.h"
---
+++ UDPSink.h
@@ -0,0 +1,14 @@
+//
+// Created by tomas on 2/4/19.
+//
+
+#ifndef CMPXCHG_UDPSINK_H
+#define CMPXCHG_UDPSINK_H
+
+
+class UDPSink {
+
+};
+
+
+#endif //CMPXCHG_UDPSINK_H
